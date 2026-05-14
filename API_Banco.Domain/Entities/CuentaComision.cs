9ee9e672194965f1b56e7606c29c5a74a8c4be88
Angel Gustavo Sanchez Rodas <asanchezr12@miumg.edu.gt>
2026-05-13 21:25:32 -0600

    Correcion en las entitdades

---
+++ API_Banco.Domain/Entities/CuentaComision.cs
@@ -0,0 +1,9 @@
+﻿namespace API_Banco.Domain.Entities
+{
+    public class CuentaComision
+    {
+        public int IdCuentaComision { get; set; }
+        public required string NombreCuenta { get; set; } // Ej: "COMISION_SERVICIOS"
+        public decimal SaldoAcumulado { get; set; }
+    }
+}
--- API_Banco.Domain/Entities/TarjetaDebito.cs
+++ API_Banco.Domain/Entities/TarjetaDebito.cs
@@ -1,16 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace API_Banco.Domain.Entities
 {
-    internal class TarjetaDebito
+    public class TarjetaDebito // <-- Cambiado a public
     {
         public int IdTarjeta { get; set; }
         public required string NumeroTarjeta { get; set; }
-        public required string Pin { get; set; }
+        public required string PinHash { get; set; } // <-- Mejor PinHash que Pin
         public DateTime FechaVencimiento { get; set; }
         public int IdCuenta { get; set; }
         public int IdEstado { get; set; }
+
+        // Navegación (¡Agregado!)
+        public virtual Cuenta? Cuenta { get; set; }
+        public virtual Estado? Estado { get; set; }
     }
 }
