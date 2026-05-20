ac1aa0f308972d6c7f03f65afdea94afcbb106be
Angel Gustavo Sanchez Rodas <asanchezr12@miumg.edu.gt>
2026-05-19 22:58:09 -0600

    Prueba1

--- API_Banco/Program.cs
+++ API_Banco/Program.cs
@@ -33,7 +33,7 @@ namespace API_Banco
 
             // 3. Registrar el DbContext con Pomelo MySQL
             builder.Services.AddDbContext<BancoDbContext>(options =>
-                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 32)))
             );
 
             builder.Services.AddControllers();
